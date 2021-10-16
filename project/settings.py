124c3e270133cf821fc32e298ecc06f2174ff8ce
aadill365 <mail2adil365@gmail.com>
2021-10-16 22:05:49 +0530

    Update settings.py
    updated settings

--- project/settings.py
+++ project/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mail2adil365@gmail.com'
-EMAIL_HOST_PASSWORD = 'secwydcbqdyxpinu'
+EMAIL_HOST_USER = "" # your email
+EMAIL_HOST_PASSWORD = "" # your password
